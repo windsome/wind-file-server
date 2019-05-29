a4e25b6e74f1e4926c9c69586c567e9afda65f24
windsome.feng <36081189@qq.com>
2019-05-30 00:48:54 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 # bash
 set -e
-export VERSION=1.0.4
+VERSION=`awk -F"\"" '/version/{print $4}' package.json`
+echo '开始编译及打包:'$VERSION
+
+# export VERSION=1.0.5
 npm run build
 docker build . -t windsome/uploader:$VERSION
 docker tag windsome/uploader:$VERSION windsome/uploader:latest
+docker login --username=86643838@163.com --password=a12345678 registry.cn-shanghai.aliyuncs.com
 docker push windsome/uploader:$VERSION
 docker push windsome/uploader:latest
